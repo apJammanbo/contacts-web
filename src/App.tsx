f135c18a61f9604a618e20e639c7da99ca41a487
apjammanbo <apjammanbo@gmail.com>
2021-08-21 22:02:49 +0900

    완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
-import React from "react";
+import styled from 'styled-components';
+import { Suspense } from 'react';
+import LoadingBox from './Components/LoadingBox';
+import MainBox from './Components/MainBox';
 
-const App = () => <div>Contacts</div>;
+const MainContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.h1`
+  color: black;
+  font-size: large;
+  font-weight: bold;
+  margin-bottom: 15px;
+`;
+
+const App = () => {
+  return (
+    <MainContainer>
+      <Suspense fallback={<LoadingBox />}>
+        <Title>Unit6 연락처</Title>
+        <MainBox />
+      </Suspense>
+    </MainContainer>
+  );
+};
 
 export default App;
